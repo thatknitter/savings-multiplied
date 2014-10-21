deee3daf3c9b99cfc38735efa7ab850638d871f1
thatknitter <kblewis.flt@gmail.com>
2014-10-21 10:37:45 -0500

    file updates

--- javascript-jquery/login.js
+++ javascript-jquery/login.js
@@ -1,21 +1,77 @@
+//function hide(element) {
+//	element.style.display = "none";
+//}
+//
+//function show(element) {
+//	element.style.display = "block";
+//}
+
 function hide(element) {
-	element.style.display = "none";
+	if (element.classList)
+		element.classList.add('hidden');
+	else
+		element.className += '' + 'hidden';
 }
 
 function show(element) {
-	element.style.display = "block";
+	if (element.classList)
+	element.classList.remove('hidden');
+	else
+	element.className = element.hidden
+		.replace(new RegExp('(^|\\b)' + 'hidden'.split('')
+		.join('|') + '(\\b|$)', 'gi'), '');
 }
 
-onclick="show(document.querySelector('.login-modal'));";
+function toggle(element) {
+	if (element.classList)
+		element.classList.toggle('hidden');
+}
+
+//onclick="show(document.querySelector('.login-modal'));";
 
 hide(document.querySelector('.login-modal'));
  
 //show(document.querySelector('.login-modal'));
- 
+
+document.addEventListener('DOMContentLoaded', function(){
 var loginModal = document.querySelector('.login-modal');
 
 var lightbox = document.querySelector('.lightbox');
 
-lightbox.addEventListener('click', function(){
-	hide(document.querySelector('.login-modal'));
+var signIn = document.querySelector('.signin');
+
+var cancel = document.querySelector('.cancel');
+
+var loginModalToggleAreas = document.querySelectorAll(
+	'.lightbox, .signin, .cancel');
+	
+	for (var i=0; i<loginModalToggleAreas.length; i++){
+	loginModalToggleAreas[i].addEventListener('click', function(){
+		toggle(loginModal);
+	});
+}
 });
+ 
+
+
+//loginModalToggleAreas.forEach(function(area){
+//	area.addEventListener('click', function(){
+//	toggle(loginModal);
+//});
+//
+//});
+
+
+
+
+//lightbox.addEventListener('click', function(){
+//	toggle(document.querySelector('.login-modal'));
+//});
+//
+//signIn.addEventListener('click', function(){
+//	toggle(document.querySelector('.login-modal'));
+//});
+//
+//cancel.addEventListener('click', function(){
+//	toggle(document.querySelector('.login-modal'));
+//});
